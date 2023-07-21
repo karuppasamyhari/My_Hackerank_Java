90644a9dcba13b9e2ee9f8b1c4fcc2ca5012bfa0
KARUPPASAMYHARIHARAN . A <86235041+karuppasamyhari@users.noreply.github.com>
2023-07-21 18:16:20 +0530

    Create Solution.java

---
+++ Java String Reverse/Solution.java
@@ -0,0 +1,40 @@
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+Given a string A, print Yes if it is a palindrome, print No otherwise.
+
+Constraints:
++ A will consist at most 50 lower case english letters.
+
+Sample Input:
+madam
+  
+Sample Output:
+Yes
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String B = new String();
+        for(int i = A.length() - 1; i >= 0; i--){
+            B = B + A.charAt(i);
+        }
+        //System.out.println(B);
+        if(B.equals(A)){
+            System.out.println("Yes");
+        }
+        else{
+             System.out.println("No");
+        }
+       
+        
+    }
+}
+
+
+
